7d1f4eca01780820c3ea4e60beeebfab94f26471
Noam Z <nzoam93@gmail.com>
2022-11-03 16:24:58 -0700

    added associations to project

--- app/models/course.rb
+++ app/models/course.rb
@@ -14,7 +14,22 @@ class Course < ApplicationRecord
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Enrollment
-    
-    
-end
 
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+
+    belongs_to :prerequisite, #refers back to the same class
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true
+    #do we needs a has_one for this?? No.
+    #associations are methods that you can use.
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+
+end
